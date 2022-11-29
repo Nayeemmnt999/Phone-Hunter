1f9f1ed3c656235acd07eee4e17899cfd2ce2a9f
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2022-11-29 09:57:01 +0600

    solved

--- script.js
+++ script.js
@@ -17,12 +17,14 @@ const phoneData = phones => {
         const createDiv = document.createElement('div');
         createDiv.classList.add('col');
         createDiv.innerHTML = `
-        <div class="card">
+        <div class="card p-4 ">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button class = "btn btn-primary">Buy Now </button>
         </div>
+        
       </div>
         `;
         display.appendChild(createDiv)
