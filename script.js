f22d8cab3e3b9c09c5cd7f2703b3451a4f0c801d
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2022-11-29 09:38:53 +0600

    disply phones

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('my name is nayeem rahman farhad');
+// const loadData = (searchText) => {
+//     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+//     .then(res => res.json())
+//     .then(data => phoneData(data.data))
+// }
+const loadData = async(searchText) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=phone`
+    const res = await fetch(url);
+    const data = await res.json();
+    phoneData(data.data);
+}
+const phoneData = phones => {
+   
+    const display = document.getElementById('display-data');
+    phones.forEach(phone =>{
+        console.log(phone);
+        const createDiv = document.createElement('div');
+        createDiv.classList.add('col');
+        createDiv.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+        `;
+        display.appendChild(createDiv)
+    })
+   
+
+}
+loadData()
